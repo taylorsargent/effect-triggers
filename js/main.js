0a45489ebf007257c98c84c83060f59d42345a2d
Taylor Sargent <sarg0049@algonquinlive.com>
2016-04-01 09:14:24 -0400

    completed the second task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var $box = $('.box');
 var $btnshowHide = $('.btn-show-hide');
+var $diamond = $('.diamond')
+var $btnmoveIn = $('.btn-move');
 
 $btnshowHide.on('click', function () {
   $box.toggleClass('appear');
+});
+
+$btnmoveIn.on('click', function () {
+  $diamond.toggleClass('move');
 });
